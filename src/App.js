9f6a78177a7188fa34c070599ac8a6e3c53fd0eb
katherine villaparedes <86531447+katherinevilla@users.noreply.github.com>
2021-10-04 11:09:11 -0400

    adicionando

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CrudApi from './CrudApi';
+//import CrudApp from './CrudApp';
+
+
+function App() {
+  return (
+    <div>
+      <h1>useReducer</h1>
+      <hr />
+      <CrudApi />
+    
+      
+      <hr />
+     
+    </div>
+  );
+}
+
+export default App;
+
